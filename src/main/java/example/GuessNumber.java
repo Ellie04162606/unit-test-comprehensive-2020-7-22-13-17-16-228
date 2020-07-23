fe8e40964c763a9e2d83d4b7dd378e06a695856f
Ellie04162606 <1181140808@qq.com>
2020-07-23 19:44:21 +0800

    [GuessNumber][Ellie] refactor: method guess number with all tests passed

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -7,37 +7,6 @@ public class GuessNumber {
     int[] answer = {1, 2, 3, 4};
 
     public String guessNumber(int[] inputNumber) {
-        String result = "";
-        if (isCorrectAnswer(inputNumber)) {
-            return "4A0B";
-        }
-        if (isCompletelyMisMatchAnswer(inputNumber)) {
-            return "0A0B";
-        }
-        return isAllNumberCorrectWithCompletelyMisMatchPlace(inputNumber);
-    }
-
-    public boolean isCorrectAnswer(int[] inputNumber) {
-        return Arrays.equals(inputNumber, answer);
-    }
-
-    public boolean isCompletelyMisMatchAnswer(int[] inputNumber) {
-        int a = 0;
-        int b = 0;
-        for (int index = 0; index < inputNumber.length; index++) {
-            if (inputNumber[index] == answer[index]) {
-                a++;
-            } else {
-                int finalIndex = index;
-                if (IntStream.of(answer).anyMatch(x -> x == inputNumber[finalIndex])) {
-                    b++;
-                }
-            }
-        }
-        return a == 0 && b == 0;
-    }
-
-    public String isAllNumberCorrectWithCompletelyMisMatchPlace(int[] inputNumber) {
         int a = 0;
         int b = 0;
         for (int index = 0; index < inputNumber.length; index++) {
