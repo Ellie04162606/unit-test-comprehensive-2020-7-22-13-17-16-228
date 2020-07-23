21a509aa98a5060096c82d6a680e6821ee7633b7
Ellie04162606 <1181140808@qq.com>
2020-07-23 18:59:58 +0800

    [GuessNumber][Ellie] test:should_return_0A0B_when_guess_number_given_completely_mismatched_number

--- src/test/java/example/GuessNumberTest.java
+++ src/test/java/example/GuessNumberTest.java
@@ -17,4 +17,16 @@ public class GuessNumberTest {
         //then
         assertEquals("4A0B",result);
     }
+
+    @Test
+    void should_return_0A0B_when_guess_number_given_completely_mismatched_number(){
+        //given
+        int[] inputNumber = {5,6,7,8};
+        GuessNumber guessNumberGame = new GuessNumber();
+
+        //when
+        String result = guessNumberGame.guessNumber(inputNumber);
+        //then
+        assertEquals("0A0B",result);
+    }
 }
