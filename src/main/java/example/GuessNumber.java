b5fa9ba8b3c32e5388e1c6ed67f6ad89b1935bd0
Ellie04162606 <1181140808@qq.com>
2020-07-23 18:54:00 +0800

    [GuessNumber][Ellie] Pass:should_return_4A0B_when_guess_number_given_correct_number test case

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -1,8 +1,19 @@
 package example;
 
+import java.util.Arrays;
+
 public class GuessNumber {
+    int[] answer = {1, 2, 3, 4};
+
     public String guessNumber(int[] inputNumber) {
         String result = "";
+        if (isCorrectAnswer(inputNumber)) {
+            return "4A0B";
+        }
         return result;
+    }
+
+    public boolean isCorrectAnswer(int[] inputNumber) {
+        return Arrays.equals(inputNumber, answer);
     }
 }
